ca0ead254f1b1bb015a300defec0dc84084436b9
coolfaiz <coolfaiz365@gmail.com>
2024-01-07 11:59:01 +0500

    Order tracking page both versions done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      '3xl': '1400px',
+    },
     extend: {},
   },
   plugins: [],
