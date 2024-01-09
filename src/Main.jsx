f7e1bb5f0cbd8a7c788adaff01ba44519fbfd0ad
Mansoor Ahmed Memon <mansoorahmed.one@gmail.com>
2024-01-09 19:23:46 +0500

    Style payment form

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,17 +3,12 @@ import ReactDOM from "react-dom/client";
 import { CookiesProvider } from "react-cookie";
 
 import "./App.css"
-import { ThemeProvider } from "@material-tailwind/react";
 import App from "./App"
-
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <CookiesProvider>
-        <App />
-      </CookiesProvider>
-    </ThemeProvider>
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
   </React.StrictMode>
 );
